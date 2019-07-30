d886f622fc1fe48d0bd89d1766caf2004ea484e6
Bharath Dasaraju <bharath@hooq.tv>
2019-07-30 11:51:04 +0800

    How declare the lists implicitly and explicitly

--- ec2_example/variables.tf
+++ ec2_example/variables.tf
@@ -1,3 +1,12 @@
  variable "bkregion" {
    type = "string"
  }
+
+# Declare lists implicitly by using brackets
+variable "cidr_1" {
+  default = []
+}
+# Declare lists explicitly using the list keyword
+variable "cidr_2" {
+  type = "list"
+}
